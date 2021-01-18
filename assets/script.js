af19bc9e98f2e509bbbf4ae2da1aff45eb3984f7
rsowald <72467920+rsowald@users.noreply.github.com>
2021-01-18 01:27:07 -0800

    added reset queryselector and quiz questions

--- assets/script.js
+++ assets/script.js
@@ -4,8 +4,7 @@ var quizEl = document.querySelector("#quiz");
 var startEl = document.querySelector("#start");
 var endEl = document.querySelector("#end");
 var correctnessEl = document.querySelector("#correctness");
-
-//TODO: add query selectors for high scores page
+var resetHighscores = document.querySelector("#reset");
 
 //TODO: fetch high scores from local storage and display on high scores page
 
@@ -16,7 +15,7 @@ document.querySelector("#startBtn").addEventListener("click", function () {
     setTime()
 });
 
-var secondsLeft = 60;
+var secondsLeft = 100;
 var timerInterval;
 
 function setTime() {
@@ -31,7 +30,58 @@ function setTime() {
     }, 1000);
 }
 
-
+var questions = [
+    {
+        q: "How do you declare a function in javascript?",
+        a: ["myFunction()", "function = myFunction()", "function: myFunction", "function myFunction()"],
+        key: 3
+    },
+    {
+        q: "What method would add a new item to the beginning of an array?",
+        a: ["unshift", "push", "pop", "replace"],
+        key: 0
+    },
+    {
+        q: "How do you declare a variable as an object?",
+        a: ["var myVariable = [1,2,3]", "var myVariable = {a:1 ,b:2, c:3}", "myVariable = (1,2,3)", "var myVariable{1;2;3}"],
+        key: 1
+    },
+    {
+        q: "How would you write 'Do you like this question?' to a confirm box?",
+        a: ["confirmBox('Do you like this question?')", "msg('Do you like this question?')", "confirm('Do you like this question?')", "alert('Do you like this question?"],
+        key: 2
+    },
+    {
+        q: "How do you start writing a For loop?",
+        a: ["for(i<arr.length; i++)", "for(i=0; i<arr.length; i++)", "for(myFunction)", "foreach(function())"],
+        key: 1
+    },
+    {
+        q: "A variable defined inside a function has what kind of scope?",
+        a: ["Global", "Loop", "Local", "Micro"],
+        key: 2
+    },
+    {
+        q: "In the code 'element.addEventListener('click', myFunction), which is the event handler?",
+        a: ["click", "element", "addEventListener", "myFunction"],
+        key: 3
+    },
+    {
+        q: "How would you call the function myFunction?",
+        a: ["myFunction()", "call myFunction()", "myFunction", "run myFunction"],
+        key: 0
+    },
+    {
+        q: "Which of these is a method for getting a reference to a DOM element?",
+        a: ["getElementById", "querySelector('#id')", "var = h1(#id)", "BOTH getElementById and querySelector('#id')"],
+        key: 3
+    },
+    {
+        q: "Which of these is a valid Boolean value?",
+        a: ["true", "4", "NaN", "To be or not to be?"],
+        key: 0
+    },
+];
 //TODO: on click in quiz div, switch to next question
 //TODO: on click in quiz div, check for correct answer and display correctness div
 //TODO: decrement seconds left for wrong answer
@@ -40,12 +90,13 @@ function setTime() {
 // Calls function to show game over div
 function endGame() {
     //TODO: hide quiz div and show end game div
+    //TODO: show high scores page/modal
     // Stops timer
     clearInterval(timerInterval);
 }
 
 //TODO: grab user info from end game div and write to local storage
-//TODO: show high scores page/modal
+
 //TODO: on click in high scores page, clear local storage
 //TODO: on click in high scores page, go back to landing div
 
