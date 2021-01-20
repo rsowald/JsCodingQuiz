ca0a4c0183564b1ece35e0d03547858c4564d5d6
rsowald <72467920+rsowald@users.noreply.github.com>
2021-01-19 23:31:07 -0800

    extracted questions to external js file

---
+++ assets/questions.js
@@ -0,0 +1,52 @@
+var questions = [
+    {
+        q: "How do you declare a function in javascript?",
+        a: ["myFunction()", "function = myFunction()", "function: myFunction", "function myFunction()"],
+        key: 3
+    },
+    {
+        q: "What method would add a new item to the beginning of an array?",
+        a: ["unshift", "push", "pop", "replace"],
+        key: 0
+    },
+    {
+        q: "How do you declare a variable as an object?",
+        a: ["var myVariable = [1,2,3]", "var myVariable = {a:1 ,b:2, c:3}", "myVariable = (1,2,3)", "var myVariable{1;2;3}"],
+        key: 1
+    },
+    {
+        q: "How would you write 'Do you like this question?' to a confirm box?",
+        a: ["confirmBox('Do you like this question?')", "msg('Do you like this question?')", "confirm('Do you like this question?')", "alert('Do you like this question?"],
+        key: 2
+    },
+    {
+        q: "How do you start writing a For loop?",
+        a: ["for(i<arr.length; i++)", "for(i=0; i<arr.length; i++)", "for(myFunction)", "foreach(function())"],
+        key: 1
+    },
+    {
+        q: "A variable defined inside a function has what kind of scope?",
+        a: ["Global", "Loop", "Local", "Micro"],
+        key: 2
+    },
+    {
+        q: "In the code 'element.addEventListener('click', myFunction), which is the event handler?",
+        a: ["click", "element", "addEventListener", "myFunction"],
+        key: 3
+    },
+    {
+        q: "How would you call the function myFunction?",
+        a: ["myFunction()", "call myFunction()", "myFunction", "run myFunction"],
+        key: 0
+    },
+    {
+        q: "Which of these is a method for getting a reference to a DOM element?",
+        a: ["getElementById", "querySelector('#id')", "var = h1(#id)", "BOTH getElementById and querySelector('#id')"],
+        key: 3
+    },
+    {
+        q: "Which of these is a valid Boolean value?",
+        a: ["true", "4", "NaN", "To be or not to be?"],
+        key: 0
+    },
+];
